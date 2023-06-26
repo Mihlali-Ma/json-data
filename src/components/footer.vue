2426ce8c20d5c06dd84a743844e12adaf3b6af45
Mihlali-Ma <129970639+Mihlali-Ma@users.noreply.github.com>
2023-06-26 16:25:33 +0200

    work

---
+++ src/components/footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="footer">
+        <div class="row">
+            <div class="col"><img src="https://i.postimg.cc/W4g39wWC/png-clipart-london-logo-designer-rolex-logo-file-text-sticker-removebg-preview.png"></div>|
+            <div class="col"><img src="" alt=""></div>
+            <div class="col"><img src="" alt=""></div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .footer{
+        background-color: #242424;
+    }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="display">
+      <div class="row">
+        <div class="col"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import footer from '@/components/footer.vue'
+import cars from '@cars.json'
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,footer
+  },
+//DOM manupilation
+
+}
+for (let cars = 0; i < cars.length; cars++) {
+  
+}
+col.innerHTML+=`<div class="card" style="width: 18rem;">
+  <img src="${cars.image}" class="card-img-top">
+  <div class="card-body">
+    <h5 class="card-title">${cars.name}</h5>
+    <p class="card-text">${cars.modal}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`
+</script>
